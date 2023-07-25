d784863804f7d7f1a127a432cb464dcfb2a6b88c
e-ssemwanga <ssemwangaelise@gmail.com>
2023-07-25 17:31:05 +0100

    more work 4 u

---
+++ functions.js
@@ -0,0 +1,19 @@
+function greet(text){
+   
+    console.log('welcome to our website' + text);
+
+}
+
+console.log('rest of the code');
+
+function signUp(){
+    let name = prompt('what is your name')
+
+greet(name);
+}
+
+console.log(name);
+signUp();
+
+// the function if done on js ins't run automatically
+//it just saves but you can then use it whenever you want
