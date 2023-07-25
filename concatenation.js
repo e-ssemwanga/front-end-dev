9945867dc60f66b007f73b4a7ecb42d484953703
e-ssemwanga <ssemwangaelise@gmail.com>
2023-07-25 16:57:10 +0100

    made changes

--- concatenation.js
+++ concatenation.js
@@ -1,10 +1,11 @@
 const bank1 = 50;
 const bank2 = 100;
+//some comment
 
 const allBanks = bank1 + bank2;
 
-const user = "elise";
-const greeting = " steve jobs once said\"life is easy\"";
+const user = "elise ssemwanga";
+const greeting = " steve jobs once said\"life is easy\" ";
 
 
 
---
+++ conditions.js
@@ -0,0 +1,14 @@
+// == equal to
+// 5 == 5 TRUE
+// 5 == 4 FALSE
+
+
+// not equal
+// 4 != 3 TRUE
+// 4 != 4 FALSE
+
+// < >
+// 4 > 10  FALSE
+//40 > 10  TRUE
+//10 >= 10 TRUE
+// 10 <= 10 FALSE
