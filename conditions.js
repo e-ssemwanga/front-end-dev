6f4ba3952deb03603570c119208dd524c5213b50
e-ssemwanga <ssemwangaelise@gmail.com>
2023-07-25 17:08:43 +0100

    see i have done something, happy?

--- conditions.js
+++ conditions.js
@@ -12,3 +12,22 @@
 //40 > 10  TRUE
 //10 >= 10 TRUE
 // 10 <= 10 FALSE
+
+const acess = false;
+
+if(acess){
+console.log(' i am famous')
+}else {
+    console.log('HEY I DO NOT HAVE ACESS')
+}
+
+console.log(heyyy)
+
+
+const age = 20;
+if(age >=18){
+    console.log("you have acess")
+ 
+}else {
+    console log.("you are too young")
+}
